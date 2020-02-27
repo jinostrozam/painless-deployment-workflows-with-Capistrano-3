47c37452319b1a822b45eb9800f80e1451d9b4e5
Jaime Inostroza <bladimirosky@gmail.com>
2020-02-27 14:40:06 -0300

    first commit

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+######################################################################
+##### Deployment config
+#######################################################################
+
+
+###############################
+##### Version lock
+###############################
+
+lock '3.6.1'
+
+
+###############################
+##### Project info
+###############################
+
+set :application, 'painless-deployment-workflows-with-capistrano-3'
+set :repo_url, 'git@github.com:jinostrozam/painless-deployment-workflows-with-Capistrano-3.git'
+
+
+###############################
+##### Deploy info
+###############################
+
+set :scm, :git
+set :use_sudo, false
+
+
+###############################
+##### Further info
+###############################
+
+set :keep_releases, 3
+set :log_level, :debug
